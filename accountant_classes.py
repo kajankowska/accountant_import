2dd59a5469b65ac078fa5a5df26d898fc455e131
Kasia Jankowska <ksobinska@gmail.com>
2021-07-30 13:25:42 +0200

    products

---
+++ accountant_classes.py
@@ -0,0 +1,61 @@
+class Warehouse:
+    def __init__(self):
+        self.balance = 0
+        self.products = {}
+        self.actions = []
+
+
+def balance(self, amount, comment):
+    if self.balance + amount < 0:
+        print("Brak środków na koncie.")
+        return False
+    log = f'{"saldo"} -- {amount} -- {comment}'
+    self.actions.append(log)
+    return True
+
+
+def purchase(self, product, price, quantity):
+    if self.balnce < price * quantity:
+        print("Niewystarczająca ilość środków na zakup")
+        return False
+    if self.product in self.products:
+        self.products[product] += quantity
+    else:
+        self.products[product] = quantity
+    self.balance -= price * quantity
+    if price < 0 or quantity < 0:
+        print("Błąd! Ujemna wartość")
+    log = f'{"zakup"} -- {product} -- {price} -- {quantity}'
+    self.actions.append(log)
+    return True
+
+
+def sales(self, product, price, quantity):
+    if product not in self.products:
+        print("Towar niedostępny w magazynie.")
+        return False
+    if self.products[product] < quantity:
+        print("Niewystarczająca ilość towaru w magazynie.")
+        return False
+    else:
+        self.products[product] -= quantity
+    if price < 0 or quantity < 0:
+        print("Błąd! Ujemna wartość")
+    log = f'{"sprzedaż"} -- {product} -- {price} -- {quantity}'
+    self.actions.append(log)
+    return True
+
+
+def storage(self, product):
+    for item in product:
+        if item not in self.products:
+            self.products[item] = 0
+    for k, v in self.products.items():
+        print(f"{k}: {v}.")
+
+
+def review(self):
+    print("Liczba wykonanych operacji:", (len(self.actions)), "\nWprowadź zakres (od, do):")
+    start = int(input())
+    end = int(input())
+    print(self.actions[start:end])
