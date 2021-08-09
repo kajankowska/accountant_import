bd25c0d606b7d837317bdc47c77c0a4d5039d3bc
kajankowska <85677666+kajankowska@users.noreply.github.com>
2021-08-09 23:57:51 +0200

    update

--- zakup.py
+++ zakup.py
@@ -1,1 +1,10 @@
+from accountant_classes import Warehouse
 
+warehouse = Warehouse()
+warehouse.launching()
+warehouse.check_action()
+warehouse.end()
+
+print("\n".join(warehouse.actions))
+print(warehouse.purchase)
+print(warehouse.inputs)
